e26a567dcf0b7c49384a8d3ac95d35e38b9e38ca
Simon Pohmann <simon@pohmann.de>
2022-03-18 17:44:56 +0000

    Solve 4iii

---
+++ Elliptic Curves/4ii_computations.py
@@ -0,0 +1,63 @@
+from math import sqrt, gcd
+
+def eval(r, a, b, l, m):
+    return r * l**4 + a * l**2 * m**2 + b/r * m**4
+
+def is_square(n):
+    if n < 0:
+        return False
+    return int(sqrt(n))**2 == n
+
+def can_prove_has_sol(r, a, b):
+    for l in range(50):
+        for m in range(50):
+            if (l != 0 or m != 0) and gcd(l, m) == 1:
+                if is_square(eval(r, a, b, l, m)):
+                    return True
+    return False
+
+def can_prove_has_no_sol(r, a, b):
+    for q in [90, 82, 110]:
+        squares = { x**2 % q for x in range(q) }
+        sol_count_mod_q =  len([(l, m) 
+            for l in range(q) 
+            for m in range(q) 
+            if eval(r, a, b, l, m)%q in squares])
+        if sol_count_mod_q == 1:
+            return True
+    return False
+
+def is_nice(r, a, b):
+    return (r < 0 and a <= 0 and b >= 0) or \
+        can_prove_has_no_sol(r, a, b) or \
+        can_prove_has_sol(r, a, b)
+
+def sqrfree_factors(b):
+    pos_factors = [n for n in range(2, b + 1) if b%n == 0]
+    pos_sqrfree_factors = [n 
+        for n in pos_factors 
+        if len([m for m in pos_factors if n % m**2 == 0]) == 0]
+    return [
+        1, -1, 
+        *pos_sqrfree_factors, 
+        *[-n for n in pos_sqrfree_factors]
+    ]
+
+def is_curve_nice(a, b):
+    a1 = -2 * a
+    b1 = a**2 - 4 * b
+    for r in sqrfree_factors(b):
+        if not is_nice(r, a, b):
+            return False
+    for r in sqrfree_factors(b1):
+        if not is_nice(r, a1, b1):
+            return False
+    return True
+
+exit()
+
+for a in range(100):
+    for b in range(100):
+        if a**2 - 4 * b != 0 and len(sqrfree_factors(b)) >= 14:
+            if is_curve_nice(a, b):
+                print(a, b, len(squarefree_factors(b)))
--- Elliptic Curves/computations.sage
+++ Elliptic Curves/computations.sage
@@ -1,24 +1,7 @@
-var("x", "y", "x1", "x2", "y1", "y2", "bx", "by", "l", "v", "a2", "a4", "a6")
 
-sigma = -1
-phix = x - bx
-phiy = sigma * y + l * x - sigma * by - l * bx
-
-m = (y1 - y2)/(x1 - x2)
-strange = (v + 1)*m + m**2 - v - x1 - x2
-weierstrass = -x1 - x2 + m**2 - a2
-e1 = strange(x1 = phix(x = x1, y = y1), y1 = phiy(x = x1, y = y1), x2 = phix(x = x2, y = y2), y2 = phiy(x = x2, y = y2))
-e2 = phix(x = weierstrass)
-
-_phix = phix(x1 = 0, y1 = 1/2, x2 = 1, y2 = -1/2, l = 0, v = -1, bx = 0, by = 1/2, a2 = -1)
-_phiy = phiy(x1 = 0, y1 = 1/2, x2 = 1, y2 = -1/2, l = 0, v = -1, bx = 0, by = 1/2, a2 = -1)
-_e1 = e1(x1 = 0, y1 = 1/2, x2 = 1, y2 = -1/2, l = 0, v = -1, bx = 0, by = 1/2, a2 = -1)
-_e2 = e2(x1 = 0, y1 = 1/2, x2 = 1, y2 = -1/2, l = 0, v = -1, bx = 0, by = 1/2, a2 = -1)
-
-tangent = (3*x**2 + 2*a2*x + a4) / 2*y
-dupl_x = tangent**2 - 2*x - a2
-dupl_y = tangent * (x - dupl_x) - y
-_v = bx - dupl_x(x = bx, y = by)
-_l = (v**2  + sigma * (by - dupl_y(x = bx, y = by))) / v
-evil = (e1(v = _v, l = _l) - e2(v = _v, l = _l)) * v**2 * (x1 - x2)
-print(evil(l = 0, v = -1, bx = 0, by = 1/2, a2 = -1, a4 = 0, a6 = 1/4))
+for a in range(10):
+    for b in range(1,10):
+        if a^2 - 4*b != 0:
+            E = EllipticCurve([0, a, 0, b, 0])
+            if E.rank() == 1:
+                print(E)
